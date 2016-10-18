ab5dda614c8440c26af3f41216fc09b87db0fe2a
Anna Selivanova <foxy.eyed@gmail.com>
2016-10-18 23:53:27 +0400

    Add feature 'Authors can delete answers' and spec

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,10 +2,12 @@ FactoryGirl.define do
   factory :answer do
     body 'To be!'
     question
+    user
   end
 
   factory :invalid_answer, class: 'Answer' do
     body nil
     question nil
+    user nil
   end
 end
