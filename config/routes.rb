346ed81c3eae3406206f47cd944f2fc76e706ef2
Anna Selivanova <foxy.eyed@gmail.com>
2016-10-26 18:14:06 +0400

    Update question via ajax

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'questions#index'
 
-  resources :questions, except: [:edit, :update] do
+  resources :questions do
     resources :answers, only: [:create, :edit, :update, :destroy], shallow: true
   end
 end
