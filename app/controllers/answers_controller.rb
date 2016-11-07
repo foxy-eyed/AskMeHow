d419d3b4351bb152ca25e98c481320224fb1c485
Anna Selivanova <foxy.eyed@gmail.com>
2016-11-08 00:45:54 +0400

    Add voting to questions and answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,6 @@
 class AnswersController < ApplicationController
+  include Voted
+
   before_action :authenticate_user!
   before_action :get_answer, only: [:edit, :update, :destroy, :accept]
   before_action :get_question, only: [:create]
