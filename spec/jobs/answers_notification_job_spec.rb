c93ea319735536015936469e3003c0eb84fdec08
Selivanova Anna <foxy.eyed@gmail.com>
2016-12-19 18:23:12 +0400

    Add spec for AnswersNotificationJob

--- spec/jobs/answers_notification_job_spec.rb
+++ spec/jobs/answers_notification_job_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe AnswersNotificationJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  let(:job) { AnswersNotificationJob.perform_now(answer) }
+
+  context 'when subscriber is not an author of answer' do
+    let(:question) { create(:question) }
+    let!(:answer) { create(:answer, question: question) }
+    let!(:subscriptions) { create_list(:subscription, 2, question: question) }
+
+    it 'queues the mailer jobs' do
+      expect { job }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(question.subscriptions.size)
+    end
+
+    it 'performs job' do
+      question.subscriptions.each do |subscription|
+        expect(SubscriptionMailer).to receive(:notify).with(subscription.user, answer).and_call_original
+      end
+      job
+    end
+  end
+
+  context 'when subscriber is an author of answer' do
+    let(:question) { create(:question) }
+    let!(:answer) { create(:answer, question: question, user: question.user) }
+
+    it 'does not queue the job' do
+      expect { job }.to_not change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size)
+    end
+
+    it 'does not perform job' do
+      expect(SubscriptionMailer).to_not receive(:notify)
+      job
+    end
+  end
 end
--- spec/jobs/daily_digest_job_spec.rb
+++ spec/jobs/daily_digest_job_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe DailyDigestJob, type: :job do
+  include ActiveJob::TestHelper
+
   let!(:users) { create_list(:user, 2) }
-  let!(:job) { DailyDigestJob.perform_now }
+  let(:job) { DailyDigestJob.perform_now }
 
   context 'when new questions exist' do
-    let!(:questions) { create_list(:yesterdays_question, 2) }
+    let!(:questions) { create_list(:yesterdays_question, 2, user: users.first) }
 
-    it 'queues the job' do
+    it 'queues the mailer jobs' do
       expect { job }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(users.size)
     end
 
@@ -15,6 +17,17 @@ RSpec.describe DailyDigestJob, type: :job do
       users.each do |user|
         expect(DailyMailer).to receive(:digest).with(user, questions).and_call_original
       end
+      job
+    end
+  end
+
+  context 'when there is no new questions' do
+    it 'does not queue the job' do
+      expect { job }.to_not change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size)
+    end
+
+    it 'does not execute perform' do
+      expect(DailyMailer).to_not receive(:digest)
       job
     end
   end
